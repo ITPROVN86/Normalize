b034bcc2d3b7022da405e49f4352f3e733a5ae06
ITPROVN86 <quangltn3@fe.edu.vn>
2024-01-31 11:49:36 +0700

    First Commit

---
+++ src/view/NormalizeView.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import controller.NormalizeController;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class NormalizeView {
+
+    public NormalizeView() {
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+}
